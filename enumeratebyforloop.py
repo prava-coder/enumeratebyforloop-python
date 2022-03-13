4af65d860d8754b83269bb1c3aa25729539b59e1
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-13 12:04:52 +0530

    Add files via upload

---
+++ enumeratebyforloop.py
@@ -0,0 +1,7 @@
+#enumerate function in python
+
+#creating a list
+listss=["pravalli","raji","sam","srivani","manju","sravani"]
+#enumerate function using for loop
+for index,lists in enumerate(listss):
+    print(index,lists)
